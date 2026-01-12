3cace33b22b9e33268e17a757dffe3292c0827b6
Anjali Yadav <140296014+AnjaliYadav-04@users.noreply.github.com>
2026-01-12 23:16:21 +0530

    Container With Water problem. Two Pointers (O(n))

---
+++ Practice_Q/maxArea.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+
+        while left < right:
+            width = right - left
+            h = min(height[left], height[right])
+            area = width * h
+
+            max_water = max(max_water, area)
+
+            # Move the pointer with smaller height
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maxArea([1,8,6,2,5,4,8,3,7]))  # 49
+    print(sol.maxArea([1,1]))               # 1
