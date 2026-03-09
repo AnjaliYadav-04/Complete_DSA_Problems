18de9862edf84cb3a3b3e4feef87ee058151d0ad
Anjali Yadav <140296014+AnjaliYadav-04@users.noreply.github.com>
2026-03-09 23:24:49 +0530

    Longest Increasing Subsequence (O(n log n))

---
+++ Practice_Q/new7.py
@@ -0,0 +1,16 @@
+import bisect
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+lis = []
+
+for x in arr:
+    pos = bisect.bisect_left(lis, x)
+
+    if pos == len(lis):
+        lis.append(x)
+    else:
+        lis[pos] = x
+
+print(len(lis))
