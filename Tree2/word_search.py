f6d8b417ed9c1c8a4745205893bf02c23a59c62a
Anjali Yadav <140296014+AnjaliYadav-04@users.noreply.github.com>
2026-08-11 23:35:36 +0530

    updated

---
+++ Tree2/word_search.py
@@ -0,0 +1,13 @@
+board=[
+    ['a','b','c'],
+    ['e','f','g'],
+    ['h','i','j']
+]
+r=1
+c=1
+
+print("Current = ",board[r][c])
+print("up = ",board[r-1][c])
+print("dowm = ",board[r+1][c])
+print("right =",board[r][c+1])
+print("left =",board[r][c-1])
